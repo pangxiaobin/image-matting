f77cf7ccfa7b8d1510f47302f53b630d87f2f4a2
lanford1204 <panglaibin2013@163.com>
2024-10-13 01:08:47 +0800

    ✨ feat:update image editor and add move、zoom out 、zoom in

---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,13 @@
+const throttle = (func, limit) => {
+    let inThrottle;
+    return function () {
+      const args = arguments;
+      const context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(() => inThrottle = false, limit);
+      }
+    };
+};
+export default throttle;
